bd904b90e9e143712cbf413b87cd36a84aafd3e1
woochan0408 <tesabel48@naver.com>
2025-10-27 14:42:13 +0900

    docs: add comments

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,10 +7,12 @@ public class Application {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
 
+        //사용자 입력 및 파싱
         String carNameInput = inputView.readCarNames();
         List<Car> cars = CarFactory.createCars(carNameInput);
         int racingCount = inputView.readRacingCount();
 
+        //게임시작
         RacingGame game = new RacingGame(cars, racingCount, outputView);
         game.run();
     }
